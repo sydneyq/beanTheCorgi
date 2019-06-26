18d5497991b3fe98c916ca7aae4b600dde7a6caa
sydneyq <qrsydney@gmail.com>
2019-06-25 21:48:17 -0400

    Echo is now working without embeds and only in a hardcoded channel ID

--- cogs/announceverify.py
+++ cogs/announceverify.py
@@ -7,9 +7,21 @@ class AnnounceVerify(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def announceverify(self, ctx, *, msg):
+    async def announceverify(self, ctx):
         if 'mechanic' in [role.name for role in ctx.message.author.roles]:
-            await ctx.send('(announceverify)')
+            embed = discord.Embed(
+                color = discord.Color.teal(),
+                title = '**Welcome to Mind Café!**',
+                description = 'Be sure to read #commandments for our server rules.'
+            )
+
+            embed.set_footer(text = 'Thanks for joining our family!')
+            embed.set_thumbnail(url = 'https://cdn.discordapp.com/emojis/593214693573787654.png')
+
+            embed.add_field(name = '**You are currently unverified.**',
+            value = 'Please take a second to verify yourself by __reacting to this message with the :beanVV: emoji.__\n\nIt should already be down for you below, but if you can\'t see it or add it, message an Angel or Guardian and they can verify you manually.')
+
+            await ctx.send(embed = embed)
         else:
             await ctx.send('You don\'t have the permissions to do that!')
 
--- cogs/echo.py
+++ cogs/echo.py
@@ -1,12 +1,20 @@
 import discord
 from discord.ext import commands
+#channelID = 593153723610693632
 
 class Echo(commands.Cog):
+    channelID = 593153723610693632
 
     def __init__(self, client):
         self.client = client
 
-    global channelRef
+    @commands.command()
+    async def getEchoChannel(self, ctx):
+        if 'mods' in [role.name for role in ctx.message.author.roles] or 'mechanic' in [role.name for role in ctx.message.author.roles]:
+            global channelID
+            await ctx.send('Current echo channel: <#' + str(channelID) + '>')
+        else:
+            await ctx.send('You don\'t have the permissions to do that!')
 
     @commands.command()
     async def setEchoChannel(self, ctx, channel: discord.TextChannel):
@@ -21,18 +29,39 @@ class Echo(commands.Cog):
                 c = ctx.guild.get_channel(channel_id)
                 await ctx.send('Channel set to' + c)
             '''
-            channelRef = channel
-            await ctx.send('Channel set to <#' + str(channelRef.id) + '>!')
+
+            global channelID
+            channelID = channel.id
+            await ctx.send('Channel set to <#' + str(channelID) + '>!')
         else:
             await ctx.send('You don\'t have the permissions to do that!')
 
     @commands.Cog.listener()
-    async def on_ready(self, ctx):
-        if ('echo' in [TextChannel.name for TextChannel in ctx.guild.text_channels]):
+    async def on_message(self, message: discord.Message):
+        #yield from self.client.process_commands(message)
+        global channelID
 
+        if message.channel.name == 'echo' and ('angels' in [role.name for role in message.author.roles] or 'mechanic' in [role.name for role in message.author.roles]):
+            if not(message.content.startswith('+')): #and message.content.startswith('.'):
+
+                channelRef = self.client.get_channel(257751892241809408)
+                '''
+                embed = discord.Embed(
+                    title = 'A Mind Café Staff Member Says:',
+                    description = message.content,
+                    color = discord.Color.teal()
+                )
+                '''
+                #await discord.Client().send_message(channelRef, embed=embed)
+                await channelRef.send("**A Mind Café Staff Member Says:**\n" + message.content)#[1:])
+                #await ctx.send(embed = embed)
+
+        #if ('echo' in [TextChannel.name for TextChannel in ctx.guild.text_channels]):
+        '''
             print('Echo is ready.')
         else:
             print('No echo channel found.')
+        '''
 
 def setup(client):
     client.add_cog(Echo(client))
