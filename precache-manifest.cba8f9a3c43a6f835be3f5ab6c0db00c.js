fd15be801bbf3ccd5d1a3b2b20d2c93ad7283267
sydneyq <qrsydney@gmail.com>
2019-09-20 01:46:06 -0400

    Updates

---
+++ precache-manifest.cba8f9a3c43a6f835be3f5ab6c0db00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2581ace9f1ce60f6943436165b724a1e",
+    "url": "/Bean-the-Corgi/index.html"
+  },
+  {
+    "revision": "c85ca52b6fe169eea920",
+    "url": "/Bean-the-Corgi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ee15be3a3149f680ef27",
+    "url": "/Bean-the-Corgi/static/js/2.4ff551b6.chunk.js"
+  },
+  {
+    "revision": "c85ca52b6fe169eea920",
+    "url": "/Bean-the-Corgi/static/js/main.5016cad7.chunk.js"
+  },
+  {
+    "revision": "fdf4d02469e286121d20",
+    "url": "/Bean-the-Corgi/static/js/runtime-main.9fe5a733.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Bean-the-Corgi/static/media/logo.25bf045c.svg"
+  }
+]);
